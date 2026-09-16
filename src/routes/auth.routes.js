84098efc707eccdfae3080b137e18545c2ba846b
Yash Kumar <yashk090909@gmail.com>
2026-09-17 03:22:41 +0530

    Update auth routes to include password reset functionality

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,7 +2,7 @@ const express=require("express")
 
 const {CompanySignup}=require("../controllers/auth.controllers")
 
-const {login,getMe,logOut}=require("../controllers/login.controller");
+const {login,getMe,logout,forgotPassword,resetPassword}=require("../controllers/login.controller");
 const requireLogin = require("../middlewares/login");
 
 const router=express.Router();
@@ -11,7 +11,11 @@ router.post("/signup",CompanySignup);
 
 router.post("/login",login)
 
-router.post("/logout",logOut)
+router.post("/logout",logout)
+
+router.post("/forgotPassword",forgotPassword)
+
+router.post("/resetPassword",resetPassword);
 
 router.get("/me",requireLogin,getMe)
 
